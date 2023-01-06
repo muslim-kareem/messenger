ab25ed38a6b33e794d569b7940f7228e44a24ccb
kareem89 <muslimkareem89@icloud.com>
2023-01-06 12:43:11 +0100

    fix: Comment message

--- backend/src/main/java/com/example/backend/message/MessageRestController.java
+++ backend/src/main/java/com/example/backend/message/MessageRestController.java
@@ -20,7 +20,7 @@ public class MessageRestController {
         return service.getMessageByAuthorId(id);
     }
 
-    //muslim was here
+    // Adrian was here
     @GetMapping("/receiver/{id}")
     public Message getMessageByReceiverId(@PathVariable String id){
         return  service.getMessageByReceiverId(id);
